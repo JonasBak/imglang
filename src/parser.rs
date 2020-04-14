698d7048eedd56489f87309054a9a4ce38f91575
Jonas Bakken <jonasxb@gmail.com>
2020-04-14 15:50:07 +0200

    Make parsing a bit cleaner

--- src/parser.rs
+++ src/parser.rs
@@ -3,7 +3,12 @@ use std::iter::Peekable;
 
 trait TokenIterator<'a>: Iterator<Item = &'a Token> {
     fn peek(&mut self) -> Option<&'a Token>;
-    fn peek_t(&mut self) -> Option<&'a TokenType>;
+    fn peek_t(&mut self) -> Option<&'a TokenType> {
+        self.peek().map(|t| &t.t)
+    }
+    fn check(&mut self, ped: &dyn Fn(&TokenType) -> bool) -> bool {
+        self.peek_t().map(|t| ped(t)).unwrap_or(false)
+    }
 }
 
 impl<'a, I> TokenIterator<'a> for Peekable<I>
@@ -12,9 +17,6 @@ where
 {
     fn peek(&mut self) -> Option<&'a Token> {
         self.peek().map(|t| *t)
-    }
-    fn peek_t(&mut self) -> Option<&'a TokenType> {
-        self.peek().map(|t| &t.t)
     }
 }
 
@@ -121,14 +123,10 @@ fn parse_unary(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
 fn parse_multiplication(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
     let mut mul = parse_unary(tokens)?;
 
-    while tokens
-        .peek_t()
-        .map(|t| match t {
-            TokenType::Slash | TokenType::Star => true,
-            _ => false,
-        })
-        .unwrap_or(false)
-    {
+    while tokens.check(&|t| match t {
+        TokenType::Slash | TokenType::Star => true,
+        _ => false,
+    }) {
         mul = match tokens.next().unwrap().t {
             TokenType::Star => Box::new(Ast::Mul(mul, parse_unary(tokens)?)),
             TokenType::Slash => Box::new(Ast::Div(mul, parse_unary(tokens)?)),
@@ -141,14 +139,10 @@ fn parse_multiplication(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>
 fn parse_addition(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
     let mut add = parse_multiplication(tokens)?;
 
-    while tokens
-        .peek_t()
-        .map(|t| match t {
-            TokenType::Minus | TokenType::Plus => true,
-            _ => false,
-        })
-        .unwrap_or(false)
-    {
+    while tokens.check(&|t| match t {
+        TokenType::Minus | TokenType::Plus => true,
+        _ => false,
+    }) {
         add = match tokens.next().unwrap().t {
             TokenType::Plus => Box::new(Ast::Add(add, parse_multiplication(tokens)?)),
             TokenType::Minus => Box::new(Ast::Sub(add, parse_multiplication(tokens)?)),
@@ -161,17 +155,13 @@ fn parse_addition(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
 fn parse_comparison(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
     let mut com = parse_addition(tokens)?;
 
-    while tokens
-        .peek_t()
-        .map(|t| match t {
-            TokenType::Greater
-            | TokenType::GreaterEqual
-            | TokenType::Lesser
-            | TokenType::LesserEqual => true,
-            _ => false,
-        })
-        .unwrap_or(false)
-    {
+    while tokens.check(&|t| match t {
+        TokenType::Greater
+        | TokenType::GreaterEqual
+        | TokenType::Lesser
+        | TokenType::LesserEqual => true,
+        _ => false,
+    }) {
         com = match tokens.next().unwrap().t {
             TokenType::Greater => Box::new(Ast::G(com, parse_addition(tokens)?)),
             TokenType::GreaterEqual => Box::new(Ast::GE(com, parse_addition(tokens)?)),
@@ -186,14 +176,10 @@ fn parse_comparison(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
 fn parse_equality(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
     let mut eq = parse_comparison(tokens)?;
 
-    while tokens
-        .peek_t()
-        .map(|t| match t {
-            TokenType::BangEqual | TokenType::EqualEqual => true,
-            _ => false,
-        })
-        .unwrap_or(false)
-    {
+    while tokens.check(&|t| match t {
+        TokenType::BangEqual | TokenType::EqualEqual => true,
+        _ => false,
+    }) {
         eq = match tokens.next().unwrap().t {
             TokenType::EqualEqual => Box::new(Ast::Eq(eq, parse_comparison(tokens)?)),
             TokenType::BangEqual => Box::new(Ast::NotEq(eq, parse_comparison(tokens)?)),
