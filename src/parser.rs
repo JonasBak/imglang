56e8b71764e61b058644f429463c86db5e296aa4
Jonas Bakken <jonasxb@gmail.com>
2020-04-14 13:24:15 +0200

    Reimplement parsing to get simpler trees

--- src/parser.rs
+++ src/parser.rs
@@ -1,13 +1,6 @@
 use super::*;
 use std::iter::Peekable;
 
-trait AstNode
-where
-    Self: std::marker::Sized,
-{
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self>;
-}
-
 trait TokenIterator<'a>: Iterator<Item = &'a Token> {
     fn peek(&mut self) -> Option<&'a Token>;
     fn peek_t(&mut self) -> Option<&'a TokenType>;
@@ -30,230 +23,182 @@ pub type ParserResult<T> = Result<T, ParserError>;
 #[derive(Debug)]
 pub enum ParserError {
     UnexpectedToken(Token),
-    // For when there is a None where there shouldn't be one, TODO
-    LogicError,
+    ExpectedToken,
 }
 
-pub fn parse_tokens(tokens: Vec<Token>) -> ParserResult<Expression> {
+pub fn parse_tokens(tokens: Vec<Token>) -> ParserResult<Box<Ast>> {
     let mut tokens = tokens.iter().peekable();
-    Expression::parse(&mut tokens)
+    parse_expression(&mut tokens)
 }
 
 #[derive(Debug)]
-pub enum Primary {
+pub enum Ast {
+    // primary
     Number(f64),
     String(String),
     False,
     True,
     Nil,
-    GroupedExpr(Box<Expression>),
-}
-impl AstNode for Primary {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        match tokens.peek_t().ok_or(ParserError::LogicError)? {
-            TokenType::Number(n) => {
-                tokens.next();
-                Ok(Primary::Number(*n))
-            }
-            TokenType::String(string) => {
-                tokens.next();
-                Ok(Primary::String(string.clone()))
-            }
-            TokenType::False => {
-                tokens.next();
-                Ok(Primary::False)
-            }
-            TokenType::True => {
-                tokens.next();
-                Ok(Primary::True)
-            }
-            TokenType::Nil => {
-                tokens.next();
-                Ok(Primary::Nil)
-            }
-            TokenType::LeftPar => {
-                tokens.next();
-                let node = Primary::GroupedExpr(Box::new(Expression::parse(tokens)?));
-                match tokens.peek_t() {
-                    Some(TokenType::RightPar) => {
-                        tokens.next();
-                        Ok(node)
-                    }
-                    _ => Err(ParserError::UnexpectedToken(
-                        tokens.next().ok_or(ParserError::LogicError)?.clone(),
-                    )),
-                }
-            }
+    GroupedExpr(Box<Ast>),
 
-            _ => Err(ParserError::UnexpectedToken(tokens.next().unwrap().clone())),
+    // unary
+    Bang(Box<Ast>),
+    Negated(Box<Ast>),
+
+    // binary
+    Mul(Box<Ast>, Box<Ast>),
+    Div(Box<Ast>, Box<Ast>),
+
+    Add(Box<Ast>, Box<Ast>),
+    Sub(Box<Ast>, Box<Ast>),
+
+    G(Box<Ast>, Box<Ast>),
+    GE(Box<Ast>, Box<Ast>),
+    L(Box<Ast>, Box<Ast>),
+    LE(Box<Ast>, Box<Ast>),
+
+    Eq(Box<Ast>, Box<Ast>),
+    Not(Box<Ast>, Box<Ast>),
+}
+
+fn parse_primary(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    match tokens.peek_t().ok_or(ParserError::ExpectedToken)? {
+        TokenType::Number(n) => {
+            tokens.next();
+            Ok(Box::new(Ast::Number(*n)))
         }
-    }
-}
-
-#[derive(Debug)]
-pub enum Unary {
-    Bang(Box<Unary>),
-    Negated(Box<Unary>),
-    Primary(Box<Primary>),
-}
-impl AstNode for Unary {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        let unary = match tokens.peek_t() {
-            Some(TokenType::Bang) => {
-                tokens.next();
-                Unary::Bang(Box::new(Unary::parse(tokens)?))
-            }
-            Some(TokenType::Minus) => {
-                tokens.next();
-                Unary::Negated(Box::new(Unary::parse(tokens)?))
-            }
-            _ => Unary::Primary(Box::new(Primary::parse(tokens)?)),
-        };
-        Ok(unary)
-    }
-}
-
-#[derive(Debug)]
-pub enum Multiplication {
-    Mul(Box<Unary>, Option<Box<Multiplication>>),
-    Div(Box<Unary>, Option<Box<Multiplication>>),
-}
-impl AstNode for Multiplication {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        let mut mul = Multiplication::Mul(Box::new(Unary::parse(tokens)?), None);
-
-        while tokens
-            .peek_t()
-            .map(|t| match t {
-                TokenType::Slash | TokenType::Star => true,
-                _ => false,
-            })
-            .unwrap_or(false)
-        {
-            mul = match tokens.next().unwrap().t {
-                TokenType::Star => {
-                    Multiplication::Mul(Box::new(Unary::parse(tokens)?), Some(Box::new(mul)))
+        TokenType::String(string) => {
+            tokens.next();
+            Ok(Box::new(Ast::String(string.clone())))
+        }
+        TokenType::False => {
+            tokens.next();
+            Ok(Box::new(Ast::False))
+        }
+        TokenType::True => {
+            tokens.next();
+            Ok(Box::new(Ast::True))
+        }
+        TokenType::Nil => {
+            tokens.next();
+            Ok(Box::new(Ast::Nil))
+        }
+        TokenType::LeftPar => {
+            tokens.next();
+            let node = Ast::GroupedExpr(parse_expression(tokens)?);
+            match tokens.peek_t() {
+                Some(TokenType::RightPar) => {
+                    tokens.next();
+                    Ok(Box::new(node))
                 }
-                TokenType::Slash => {
-                    Multiplication::Div(Box::new(Unary::parse(tokens)?), Some(Box::new(mul)))
-                }
-                _ => return Err(ParserError::LogicError),
+                _ => Err(ParserError::UnexpectedToken(tokens.next().unwrap().clone())),
             }
         }
-        Ok(mul)
+
+        _ => Err(ParserError::UnexpectedToken(tokens.next().unwrap().clone())),
     }
 }
 
-#[derive(Debug)]
-pub enum Addition {
-    Add(Box<Multiplication>, Option<Box<Addition>>),
-    Sub(Box<Multiplication>, Option<Box<Addition>>),
-}
-impl AstNode for Addition {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        let mut add = Addition::Add(Box::new(Multiplication::parse(tokens)?), None);
-
-        while tokens
-            .peek_t()
-            .map(|t| match t {
-                TokenType::Minus | TokenType::Plus => true,
-                _ => false,
-            })
-            .unwrap_or(false)
-        {
-            add = match tokens.next().unwrap().t {
-                TokenType::Plus => Addition::Add(
-                    Box::new(Multiplication::parse(tokens)?),
-                    Some(Box::new(add)),
-                ),
-                TokenType::Minus => Addition::Sub(
-                    Box::new(Multiplication::parse(tokens)?),
-                    Some(Box::new(add)),
-                ),
-                _ => return Err(ParserError::LogicError),
-            }
+fn parse_unary(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    let unary = match tokens.peek_t() {
+        Some(TokenType::Bang) => {
+            tokens.next();
+            Box::new(Ast::Bang(parse_unary(tokens)?))
         }
-        Ok(add)
-    }
-}
-
-#[derive(Debug)]
-pub enum Comparison {
-    G(Box<Addition>, Option<Box<Comparison>>),
-    GE(Box<Addition>, Option<Box<Comparison>>),
-    L(Box<Addition>, Option<Box<Comparison>>),
-    LE(Box<Addition>, Option<Box<Comparison>>),
-}
-impl AstNode for Comparison {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        let mut com = Comparison::G(Box::new(Addition::parse(tokens)?), None);
-
-        while tokens
-            .peek_t()
-            .map(|t| match t {
-                TokenType::Greater
-                | TokenType::GreaterEqual
-                | TokenType::Lesser
-                | TokenType::LesserEqual => true,
-                _ => false,
-            })
-            .unwrap_or(false)
-        {
-            com = match tokens.next().unwrap().t {
-                TokenType::Greater => {
-                    Comparison::G(Box::new(Addition::parse(tokens)?), Some(Box::new(com)))
-                }
-                TokenType::GreaterEqual => {
-                    Comparison::GE(Box::new(Addition::parse(tokens)?), Some(Box::new(com)))
-                }
-                TokenType::Lesser => {
-                    Comparison::L(Box::new(Addition::parse(tokens)?), Some(Box::new(com)))
-                }
-                TokenType::LesserEqual => {
-                    Comparison::LE(Box::new(Addition::parse(tokens)?), Some(Box::new(com)))
-                }
-                _ => return Err(ParserError::LogicError),
-            }
+        Some(TokenType::Minus) => {
+            tokens.next();
+            Box::new(Ast::Negated(parse_unary(tokens)?))
         }
-        Ok(com)
-    }
+        _ => parse_primary(tokens)?,
+    };
+    Ok(unary)
 }
 
-#[derive(Debug)]
-pub enum Equality {
-    Eq(Box<Comparison>, Option<Box<Equality>>),
-    Not(Box<Comparison>, Option<Box<Equality>>),
-}
-impl AstNode for Equality {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        let mut eq = Equality::Eq(Box::new(Comparison::parse(tokens)?), None);
+fn parse_multiplication(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    let mut mul = parse_unary(tokens)?;
 
-        while tokens
-            .peek_t()
-            .map(|t| match t {
-                TokenType::BangEqual | TokenType::EqualEqual => true,
-                _ => false,
-            })
-            .unwrap_or(false)
-        {
-            eq = match tokens.next().unwrap().t {
-                TokenType::EqualEqual => {
-                    Equality::Eq(Box::new(Comparison::parse(tokens)?), Some(Box::new(eq)))
-                }
-                TokenType::BangEqual => {
-                    Equality::Not(Box::new(Comparison::parse(tokens)?), Some(Box::new(eq)))
-                }
-                _ => return Err(ParserError::LogicError),
-            }
+    while tokens
+        .peek_t()
+        .map(|t| match t {
+            TokenType::Slash | TokenType::Star => true,
+            _ => false,
+        })
+        .unwrap_or(false)
+    {
+        mul = match tokens.next().unwrap().t {
+            TokenType::Star => Box::new(Ast::Mul(mul, parse_unary(tokens)?)),
+            TokenType::Slash => Box::new(Ast::Div(mul, parse_unary(tokens)?)),
+            _ => panic!(),
         }
-        Ok(eq)
     }
+    Ok(mul)
 }
 
-#[derive(Debug)]
-pub struct Expression(Box<Equality>);
-impl AstNode for Expression {
-    fn parse(tokens: &mut dyn TokenIterator) -> ParserResult<Self> {
-        Ok(Expression(Box::new(Equality::parse(tokens)?)))
+fn parse_addition(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    let mut add = parse_multiplication(tokens)?;
+
+    while tokens
+        .peek_t()
+        .map(|t| match t {
+            TokenType::Minus | TokenType::Plus => true,
+            _ => false,
+        })
+        .unwrap_or(false)
+    {
+        add = match tokens.next().unwrap().t {
+            TokenType::Plus => Box::new(Ast::Add(add, parse_multiplication(tokens)?)),
+            TokenType::Minus => Box::new(Ast::Sub(add, parse_multiplication(tokens)?)),
+            _ => panic!(),
+        }
     }
+    Ok(add)
+}
+
+fn parse_comparison(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    let mut com = parse_addition(tokens)?;
+
+    while tokens
+        .peek_t()
+        .map(|t| match t {
+            TokenType::Greater
+            | TokenType::GreaterEqual
+            | TokenType::Lesser
+            | TokenType::LesserEqual => true,
+            _ => false,
+        })
+        .unwrap_or(false)
+    {
+        com = match tokens.next().unwrap().t {
+            TokenType::Greater => Box::new(Ast::G(com, parse_addition(tokens)?)),
+            TokenType::GreaterEqual => Box::new(Ast::GE(com, parse_addition(tokens)?)),
+            TokenType::Lesser => Box::new(Ast::L(com, parse_addition(tokens)?)),
+            TokenType::LesserEqual => Box::new(Ast::LE(com, parse_addition(tokens)?)),
+            _ => panic!(),
+        }
+    }
+    Ok(com)
+}
+
+fn parse_equality(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    let mut eq = parse_comparison(tokens)?;
+
+    while tokens
+        .peek_t()
+        .map(|t| match t {
+            TokenType::BangEqual | TokenType::EqualEqual => true,
+            _ => false,
+        })
+        .unwrap_or(false)
+    {
+        eq = match tokens.next().unwrap().t {
+            TokenType::EqualEqual => Box::new(Ast::Eq(eq, parse_comparison(tokens)?)),
+            TokenType::BangEqual => Box::new(Ast::Not(eq, parse_comparison(tokens)?)),
+            _ => panic!(),
+        }
+    }
+    Ok(eq)
+}
+
+fn parse_expression(tokens: &mut dyn TokenIterator) -> ParserResult<Box<Ast>> {
+    parse_equality(tokens)
 }
